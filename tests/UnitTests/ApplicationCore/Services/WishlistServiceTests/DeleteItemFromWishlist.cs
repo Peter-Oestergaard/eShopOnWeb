1a0c03784165cb06231b0ffbae741410085c780c
Peter Østergaard <peter@engelund.eu>
2023-03-05 20:38:45 +0100

    Begun last test

--- tests/UnitTests/ApplicationCore/Services/WishlistServiceTests/DeleteItemFromWishlist.cs
+++ tests/UnitTests/ApplicationCore/Services/WishlistServiceTests/DeleteItemFromWishlist.cs
@@ -50,4 +50,22 @@ public class DeleteItemFromWishlist
         // Act & Assert
         await Assert.ThrowsAsync<InvalidOperationException>(() => wishlistService.RemoveItemFromWishlist(wishlist.BuyerId, catalogItemId));
     }
+
+    [Theory]
+    [ClassData(typeof(DeleteItemsDataGenerator))]
+    public async Task WhenRemovingSomeItemFromWishlistService_RestIsStillThere(List<int> catalogItemIds, List<int> itemsToRemove, List<int> remainingItems)
+    {
+        /*
+        // Arrange
+        var mockWishlist = new Mock<Wishlist>(_buyerId);
+        var wishlist = mockWishlist.Object;
+        Assert.Empty(wishlist.Items);
+        _mockWishlistRepo.Setup(x => x.FirstOrDefaultAsync(It.IsAny<WishlistWithItemsSpecification>(), default)).ReturnsAsync(wishlist);
+        var wishlistService = new WishlistService(_mockWishlistRepo.Object, _mockLogger.Object);
+
+        // Act & Assert
+        await Assert.ThrowsAsync<InvalidOperationException>(() => wishlistService.RemoveItemFromWishlist(wishlist.BuyerId, catalogItemId));
+        */
+        Assert.True(true);
+    }
 }
---
+++ tests/UnitTests/ApplicationCore/Services/WishlistServiceTests/DeleteItemsDataGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+
+namespace Microsoft.eShopWeb.UnitTests.ApplicationCore.Services.WishlistServiceTests;
+
+public class DeleteItemsDataGenerator : IEnumerable<object[]>
+{
+    private readonly List<object[]> data = new List<object[]>
+    {
+        new object[] {
+            new List<int> { 1, 2, 3 },
+            new List<int> { 1 },
+            new List<int> { 2, 3 } 
+        }
+    };
+
+    public IEnumerator<object[]> GetEnumerator()
+    {
+        return data.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
